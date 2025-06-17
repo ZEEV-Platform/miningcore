61d15a70c71fce1a35904a49dc2eb8459866fd90
elharari <elharari@tuta.io>
2025-06-17 09:10:00 +0200

    Fix error in API default pool value

--- src/Miningcore/Api/Controllers/PoolApiController.cs
+++ src/Miningcore/Api/Controllers/PoolApiController.cs
@@ -75,7 +75,7 @@ public class PoolApiController : ApiControllerBase
                 {
                     DateTime startTime = lastBlockTime.Value;
                     var poolEffort = await cf.Run(con => shareRepo.GetEffortBetweenCreatedAsync(con, config.Id, pool.ShareMultiplier, startTime, clock.Now));
-                    result.PoolEffort = poolEffort.Value;
+                    result.PoolEffort = poolEffort.GetValueOrDefault(0);
                 }
 
                 var from = clock.Now.AddHours(-topMinersRange);
